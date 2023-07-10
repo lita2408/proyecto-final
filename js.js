b18dd3548f3003cc383ecff1725b18c8ea5fa18e
lita2408 <paoola2808@gmail.com>
2023-07-09 22:56:48 -0600

    mensaje del commit

---
+++ js.js
@@ -0,0 +1,7 @@
+<><script>
+    function prompter() { }
+    var reply = prompt("Buenas ¿cuál es tu nombre?",)
+    alert ( "Es un gusto tenerte aquí " + reply + "!")
+    
+
+</script><button type="button" onclick="prompter()">Probar prompt</button></>
